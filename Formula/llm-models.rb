3f039f31e72191d28ac4abdff0c3852484b3be37
Ljubomir Buturovic <lbuturovic@inflammatix.com>
2025-11-24 23:12:14 -0800

    Finalize Formula

--- Formula/llm-models.rb
+++ Formula/llm-models.rb
@@ -3,8 +3,8 @@ class LlmModels < Formula
 
   desc "CLI tool to list available LLM models from various providers"
   homepage "https://github.com/ljbuturovic/llm-models"
-  url "https://files.pythonhosted.org/packages/.../llm-models-0.1.0.tar.gz"
-  sha256 "YOUR_SHA256_HERE"
+  url "https://files.pythonhosted.org/packages/89/f5/04191b54bb1b692b196d34b17cf2dce6c4059dae889e903df39c02c21280/llm_models-0.1.0.tar.gz"
+  sha256 "1265d8933231f66fee79fb1c67a1a14e6f0ee5b8af59cfa0b3317535f824fa50"
 
   depends_on "python@3.9"
 
@@ -16,3 +16,4 @@ class LlmModels < Formula
     system "#{bin}/llm-models", "--help"
   end
 end
+
